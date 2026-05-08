e664c36b264af70f05db48e6921f09074f1a6f2f
Vayu <vayumishra143@gmail.com>
2026-05-08 19:11:16 +0530

    Updated final Streamlit dashboard app

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,3 +1,16 @@
+import sys
+import os
+
+# Fix Streamlit deployment import issue
+sys.path.append(
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            ".."
+        )
+    )
+)
+
 import streamlit as st
 import plotly.express as px
 
@@ -9,17 +22,21 @@ from src.database import (
     fetch_reports
 )
 
+# Streamlit Page Configuration
 st.set_page_config(
     page_title="Email Dashboard",
     layout="wide"
 )
 
+# Dashboard Title
 st.title(
     "📧 Email Automation Dashboard"
 )
 
+# Fetch Database Reports
 df = fetch_reports()
 
+# If No Data
 if df.empty:
 
     st.warning(
@@ -28,10 +45,12 @@ if df.empty:
 
 else:
 
+    # Analytics Statistics
     stats = email_statistics(
         "outputs/report.csv"
     )
 
+    # Metrics Row
     col1, col2, col3, col4 = st.columns(4)
 
     col1.metric(
@@ -54,9 +73,17 @@ else:
         stats["failed"]
     )
 
-    st.subheader("Email Reports")
+    # Reports Table
+    st.subheader(
+        "Email Reports"
+    )
 
     st.dataframe(df)
+
+    # Pie Chart Analytics
+    st.subheader(
+        "Email Status Analytics"
+    )
 
     fig = px.pie(
         names=[
@@ -69,7 +96,17 @@ else:
             stats["dry_run"],
             stats["failed"]
         ],
-        title="Email Status Analytics"
+        title="Email Status Distribution"
     )
 
-    st.plotly_chart(fig)
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
+
+# Footer
+st.markdown("---")
+
+st.markdown(
+    "Developed using Python, Streamlit, SQLite, Pandas, and Plotly 🚀"
+)
